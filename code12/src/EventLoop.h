52a6ed4eb34935e20a9e11b0d23d212ba164b03c
zbh123456hui <18737374397@163.com>
2023-09-13 09:39:51 +0800

    main

---
+++ code12/src/EventLoop.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <functional>
+
+class Epoll;
+class Channel;
+class EventLoop
+{
+private:
+    Epoll *ep;
+    bool quit;
+public:
+    EventLoop();
+    ~EventLoop();
+
+    void loop();
+    void updateChannel(Channel*);
+
+    void addThread(std::function<void()>);
+};
---
+++ code13/src/include/Connection.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Macros.h"
+
+#include <functional>
+
+class EventLoop;
+class Socket;
+class Channel;
+class Buffer;
+class Connection {
+ public:
+  Connection(EventLoop *loop, Socket *sock);
+  ~Connection();
+  DISALLOW_COPY_AND_MOVE(Connection);
+
+  void Echo(int sockfd);
+  void SetDeleteConnectionCallback(std::function<void(int)> const &callback);
+  void Send(int sockfd);
+
+ private:
+  EventLoop *loop_;
+  Socket *sock_;
+  Channel *channel_;
+  std::function<void(int)> delete_connectioin_callback_;
+  Buffer *read_buffer_;
+};
